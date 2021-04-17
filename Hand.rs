071e75665d42dd981d7fdc5a20398cd42bce748e
NoodlePot <kazakov12231999@gmail.com>
2021-04-17 03:12:55 +0300

    Getrank changed

--- Hand.rs
+++ Hand.rs
@@ -57,10 +57,12 @@ fn get_val_hand(arr: &mut[i32]) -> &mut[i32]
 {
   for i in 0..(arr.len())
   { 
+    println!("Hand is (inside) {:?}",arr);
     arr[i] = get_val(arr[i]);  
   }
   arr
 }
+
 /*
 fn get_val_h(arr: &mut Vec<i32>) -> Vec<i32>
 {
@@ -135,6 +137,8 @@ fn has_flush(hand: &mut[i32]) -> bool
   
   for i in 0..((hand.len())-1)
   {
+    //println!("{}",get_suit(hand[i]));
+    //println!("Card {} is: {}",i,hand[i]);
     if (get_suit(hand[i]) != get_suit(hand[i+1]))
     {
      t = false;
@@ -151,7 +155,9 @@ fn has_royal_flush(hand: &mut[i32]) -> bool
   if has_flush(hand) == true {
 
   //let mut var = false;
+  println!("Hand is (outside1) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
+  
   new_hand.sort();
   if new_hand[0] == 1 
   {
@@ -171,6 +177,8 @@ fn has_royal_flush(hand: &mut[i32]) -> bool
   var
 }
 
+
+
 fn has_straight_flush(hand: &mut[i32]) -> bool
 {
   let mut var = false;
@@ -188,11 +196,15 @@ fn has_straight_flush(hand: &mut[i32]) -> bool
 fn has_straight(hand: &mut[i32]) -> bool 
 {
   let mut var = false;
+  println!("Hand is (outside2) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
+  println!("Hand is (straight) {:?}",new_hand);
   new_hand.sort();
-  //println!("{:?}",new_hand);
+  println!("Hand is (straight) {:?}",new_hand);
+  //println!("New hand is:{:?}",new_hand);
   for i in 0..((new_hand.len())-1)
   {
+    println!("Hand is (straight) {:?}",new_hand); 
   //if new_hand[i] < new_hand[i+1]
     if (new_hand[i+1] - new_hand[i] == 1)
       {
@@ -212,6 +224,7 @@ fn has_fr_of_kind(hand: &mut[i32]) -> bool
   let mut my_bool = false;
   //let mut vec = Vec::new();
  // let mut res: [i32;2] = [0,0];
+ println!("Hand is (outside3) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
   new_hand.sort();
   
@@ -247,6 +260,7 @@ fn has_fr_of_kind(hand: &mut[i32]) -> bool
 fn has_thr_of_kind(hand: &mut[i32]) -> bool
 {
   let mut my_bool = false;
+  println!("Hand is (outside4) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
   new_hand.sort();
   
@@ -270,6 +284,7 @@ fn has_thr_of_kind(hand: &mut[i32]) -> bool
 fn has_pair(hand: &mut[i32]) -> bool
 {
   let mut my_bool = false;
+  println!("Hand is (outside5) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
   new_hand.sort();
   
@@ -317,6 +332,7 @@ fn has_full_house(hand: &mut[i32]) -> bool
 fn has_two_pair(hand: &mut[i32]) -> bool 
 {
   let mut my_bool = false;
+  println!("Hand is (outside6) {:?}",hand);
   let mut new_hand = get_val_hand(hand);
   new_hand.sort();
   
@@ -354,14 +370,21 @@ fn has_two_pair(hand: &mut[i32]) -> bool
 
 fn get_ranks(hand: &mut[i32;5]) -> i32
 {
+  println!("Hand is {:?}",hand);
   let mut var: i32 = 0;
-  if has_royal_flush(hand)
+  if has_flush(hand) //== false
   {
-    var = 1;
+    //println!("{}",has_flush(hand));
+    println!("Hand is(flush) {:?}",hand);
+    var = 5;
   }
   else if has_straight_flush(hand)
   {
     var = 2;
+  }
+  else if has_royal_flush(hand)
+  {
+    var = 1;
   }
   else if has_fr_of_kind(hand)
   {
@@ -371,11 +394,7 @@ fn get_ranks(hand: &mut[i32;5]) -> i32
   {
     var = 4;
   }
-  else if has_flush(hand) == false
-  {
-    println!("{}",has_flush(hand));
-    var = 5;
-  }
+  
   else if has_straight(hand)
   {
     var = 6;
@@ -405,6 +424,7 @@ fn get_ranks(hand: &mut[i32;5]) -> i32
 
 fn main() 
 {
+  /*
   let x = 15;
   let y = 32;
   
@@ -418,8 +438,11 @@ fn main()
   //let mut r: [i32;5] = [8,23,8,51,4];
   //let mut r: [i32;5] = [40,41,48,43,51];
   //[10,32,10,5,8] pair but shown as flash
-  let mut r: [i32;5] = [10,32,10,5,8];
-  
+  let mut r: [i32;5] = [10,32,23,5,8];
+  let mut r2: [i32;5] = r.clone();
+  let g: [i32;5] = [14,23,24,25,26];
+  let test: [i32;5] = [14,23,24,25,26];
+
   println!("{}",get_suit(x));
 //  println!("{}",get_val(y));
 //  println!("{:?}",get_val_hand(&mut(z)));
@@ -427,7 +450,8 @@ fn main()
 //  println!("{}",get_let(34));
 println!("flush,{}",has_flush(&mut(h)));  
 println!("flush,{}",has_flush(&mut(r)));
-  
+
+
 //  println!("{:?}",convert([1,2,3,14,5]));
   println!("Royal Flush,{:?}", has_royal_flush(&mut(r)));
   println!("straightFlush,{:?}", has_straight_flush(&mut(r)));
@@ -440,6 +464,7 @@ println!("flush,{}",has_flush(&mut(r)));
   println!("FullHouse,{:?}", has_full_house(&mut(r)));
 
   println!("{:?}", get_ranks(&mut(h)));
-  println!("{:?}", get_ranks(&mut(r)));
-
+  println!("{:?}", r2);
+  println!("{:?}", get_ranks(&mut(r2)));
+ */
 }
